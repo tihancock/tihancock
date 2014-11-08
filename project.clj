59ec61e872ebcf81bf40d12e73a4456b5394dc35
Tom Hancock <thomas.ian.hancock@gmail.com>
2014-11-08 15:34:24 -0600

    * Remove stasis entirely * Make photo page only page * Sort out left and right arrows

--- project.clj
+++ project.clj
@@ -1,28 +1,16 @@
 (defproject tihancock "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
-                 [stasis "2.2.2"]
-                 [hiccup "1.0.5"]
                  [hiccups "0.3.0"]
                  [garden "1.2.5"]
-                 [ring "1.3.1"]
                  [cljs-http "0.1.17"]
                  [jayq "2.5.2"]]
 
   :plugins [[lein-garden "0.2.4"]
-            [lein-ring "0.8.13"]
             [lein-cljsbuild "1.0.3"]
-            [lein-auto "0.1.1"]
             [lein-pdo "0.1.1"]]
 
-  :aliases {"build-site" ["run" "-m" "tihancock.core/export"]
-            "dev"        ["pdo" ["auto" "build-site"] ["cljsbuild" "auto"] ["garden" "auto"]]}
-
-  :source-paths ["src/clj"]
-
-  :ring {:handler tihancock.core/app
-         :auto-reload? true
-         :auto-refresh? true}
+  :aliases {"dev" ["pdo" ["cljsbuild" "auto"] ["garden" "auto"]]}
 
   :garden {:builds [{:source-paths "src/clj"
                      :stylesheet tihancock.css/style
--- src/clj/tihancock/css.clj
+++ src/clj/tihancock/css.clj
@@ -15,55 +15,15 @@
                   :height   "100%"
                   :width    "100%"}]
 
-  [:.half-screen {:overflow :hidden
-                  :position :relative
-                  :width "50%"
-                  :left "25%"
-                  :right "25%"
-                  :height "100%"}]
-
-  [:.home-page-link-container {:position :relative
-                               :display :inline-block
-                               :height "100%"
-                               :width "50%"
-                               :text-align :center}]
-
-  [:.home-page-link {:position :relative
-                     :top "50%"
-                     :transform "translateY(-50%)"
-                     :display :block
-                     :text-decoration :none
-                     :color :#ADADAD
-                     :text-align :center
-                     :font-size "150%"}]
-
-  [:.home-page-link-image {:display :block
-                           :margin-left :auto
-                           :margin-right :auto
-                           :text-align :center
-                           :max-width "50%"
-                           :max-height "50%"}]
-
-  [:#sidebar {:position :absolute
-              :width "calc(20% - 2px)"
-              :height "100%"
-              :border-right "solid 2px #ADADAD"}]
-
-  [:#sidebar-link-container {:position :relative
-                             :top "50%"
-                             :transform "translateY(-50%)"}]
-
-  [:.sidebar-link {:display :block
-                   :text-decoration :none
-                   :color :#ADADAD
-                   :text-align :center
-                   :font-size "150%"}]
-
   [:#photo-container {:position :absolute
-                      :left "20%"
-                      :width "80%"
-                      :height "100%"}]
-
+                      :margin-bottom "0"
+                      :top "10%"
+                      :bottom "10%"
+                      :left "10%"
+                      :right "10%"
+                      :border "solid 5px"
+                      :border-color :#ADADAD}]
+  
   [:.photo {:position :relative
             :max-height "100%"
             :max-width "100%"
@@ -77,8 +37,13 @@
    :.slick-slide {:position :relative
                   :height "100%"}]
 
-  [:.slick-prev {:left "25px ! important"}]
-  [:.slick-next {:right "25px ! important"}]
+  [:.slick-prev {:left "40px ! important"}]
+  [:.slick-next {:right "40px ! important"}]
 
-  [:.slick-prev:before {:content "u+21E6"}]
-  [:.slick-next:before {:content "u+21E8"}])
+  [:.slick-prev :.slick-next {:height "40px ! important"
+                              :width  "40px ! important"}]
+
+  [:.slick-prev:before :.slick-next:before {:font-size "40px ! important"}]
+
+  [:.slick-prev:before {:content "\"\\25C0\" ! important"}]
+  [:.slick-next:before {:content "\"\\25B6\" ! important"}])
